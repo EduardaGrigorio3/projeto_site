bf281811368d53fa8696ce1e5790b666038cf29b
mayx2 <mmasc@discente.ifpe.edu.br>
2025-02-18 22:32:54 -0300

    Ajeitei o defeito nas opcoes de sabores e reorganizei o css de produto e de outras paginas

--- sitebolo/public/js/produtos.js
+++ sitebolo/public/js/produtos.js
@@ -5,7 +5,7 @@ async function main() {
 
     const requisicao = await fetch("http://localhost:55555/api/produtos")
     const dados = await requisicao.json()
-    console.log(dados)
+    console.log(dados[3])
     //2- obter o elemento html id produtos e construir os demais elementos
 
     const produtosBolo = document.getElementById("produtos-bolo")
@@ -13,32 +13,41 @@ async function main() {
     const produtosBiscoito = document.getElementById("produtos-biscoito")
 
     for (let x = 0; x < dados.length; x++) {
-
+        let sabores = []
+        if (dados[x].sabor != null) {
+            sabores = dados[x].sabor.split(",")
+        }
         if (dados[x].tipo === "bolo") {
+
             produtosBolo.innerHTML += `
         <div class="produto">
         <div class="imagem-container"> <input type="checkbox" id="coracao${x}" class="coracao-checkbox">
             <label for="coracao${x}" class="btn-coracao">&#9829;</label> <img
                 src="${dados[x].img}" alt="Bolo trad 1kg"> </div>
-        <h4>${dados[x].nome}</h4>
-        <h3>1Kg | R$${dados[x].preco.toFixed(2).replace('.', ',')}</h3>
-        <div class="quantidade-e-opcoes-container"
-            style="display: flex; align-items: center; gap: 10px;">
+        <div class="nome-e-preco-container">
+            <h3>${dados[x].nome}</h3>
+            <h3>R$${dados[x].preco.toFixed(2).replace('.', ',')}</h3></div>
+        <div class="quantidade-e-opcoes-container">
             <div class="quantidade-container"
-                style="display: flex; flex-direction: column; align-items: center;"> <label
-                    for="quantidade${x}"">${dados[x].unidade}</label> <input type="number"
-                    id="quantidade${x}" min="1" step="1" value="1" class="quantidade-pequena"> </div>
-            <div class="opcoes-container"
-                style="display: flex; flex-direction: column; align-items: center;"> <label
-                    for="opcao${x}">Sabores:</label> <select id="opcao${x}" class="opcoes-pequena">
+                <label
+                    for="quantidade${x}">${dados[x].unidade}</label> 
+                    <input type="number"
+                    id="quantidade${x}" min="1" step="1" value="1" class="quantidade">
+            </div>
+            <div class="opcoesSabores-container"
+                <label
+                    for="opcao${x}">Sabores:
+                </label>
+                <select id="opcao${x}" class="opcoesSabores">
                     <option value="">----</option>
-                    <option value="">${dados[x].sabor}</option>
+                    ${sabores.map(sabor => `<option value="${sabor}">${sabor}</option>`).join('')}
                 </select>
             </div>
         </div>
         <div class="botao-container">
             <button class="btn-comprar">Comprar</button>
             <button class="btn-carrinho"><i class="fa fa-shopping-cart"></i></button>
+        </div>
         </div>`
         } else if (dados[x].tipo === "caixinha") {
             produtosCaixinha.innerHTML += `
@@ -46,59 +55,60 @@ async function main() {
         <div class="imagem-container"> <input type="checkbox" id="coracao${x}" class="coracao-checkbox">
             <label for="coracao${x}" class="btn-coracao">&#9829;</label> <img
                 src="${dados[x].img}" alt="Bolo trad 1kg"> </div>
-        <h4>${dados[x].nome}</h4>
-        <h3>1Kg | R$${dados[x].preco.toFixed(2).replace('.', ',')}</h3>
-        <div class="quantidade-e-opcoes-container"
-            style="display: flex; align-items: center; gap: 10px;">
+        <div class="nome-e-preco-container"><h3>${dados[x].nome}</h3>
+        <h3>R$${dados[x].preco.toFixed(2).replace('.', ',')}</h3></div>
+        <div class="quantidade-e-opcoes-container">
             <div class="quantidade-container"
-                style="display: flex; flex-direction: column; align-items: center;"> <label
-                    for="quantidade${x}"">${dados[x].unidade}</label> <input type="number"
-                    id="quantidade${x}" min="1" step="1" value="1" class="quantidade-pequena"> </div>
-            <div class="opcoes-container"
-                style="display: flex; flex-direction: column; align-items: center;"> <label
-                    for="opcao${x}">Sabores:</label> <select id="opcao${x}" class="opcoes-pequena">
+                <label
+                    for="quantidade${x}">${dados[x].unidade}</label> 
+                    <input type="number"
+                    id="quantidade${x}" min="1" step="1" value="1" class="quantidade">
+            </div>
+            <div class="opcoesSabores-container"
+                <label
+                    for="opcao${x}">Sabores:
+                </label>
+                <select id="opcao${x}" class="opcoesSabores">
                     <option value="">----</option>
-                    <option value="">${dados[x].sabor}</option>
+                    ${sabores.map(sabor => `<option value="${sabor}">${sabor}</option>`).join('')}
                 </select>
             </div>
         </div>
         <div class="botao-container">
             <button class="btn-comprar">Comprar</button>
             <button class="btn-carrinho"><i class="fa fa-shopping-cart"></i></button>
+        </div>
         </div>`
-
-        }
-        else {
+        } else {
             produtosBiscoito.innerHTML += `
         <div class="produto">
         <div class="imagem-container"> <input type="checkbox" id="coracao${x}" class="coracao-checkbox">
             <label for="coracao${x}" class="btn-coracao">&#9829;</label> <img
                 src="${dados[x].img}" alt="Bolo trad 1kg"> </div>
-        <h4>${dados[x].nome}</h4>
-        <h3>1Kg | R$${dados[x].preco.toFixed(2).replace('.', ',')}</h3>
-        <div class="quantidade-e-opcoes-container"
-            style="display: flex; align-items: center; gap: 10px;">
+        <div class="nome-e-preco-container"><h3>${dados[x].nome}</h3>
+        <h3>R$${dados[x].preco.toFixed(2).replace('.', ',')}</h3></div>
+        <div class="quantidade-e-opcoes-container">
             <div class="quantidade-container"
-                style="display: flex; flex-direction: column; align-items: center;"> <label
-                    for="quantidade${x}"">${dados[x].unidade}</label> <input type="number"
-                    id="quantidade${x}" min="1" step="1" value="1" class="quantidade-pequena"> </div>
-            <div class="opcoes-container"
-                style="display: flex; flex-direction: column; align-items: center;"> <label
-                    for="opcao${x}">Sabores:</label> <select id="opcao${x}" class="opcoes-pequena">
+                <label
+                    for="quantidade${x}">${dados[x].unidade}</label> 
+                    <input type="number"
+                    id="quantidade${x}" min="1" step="1" value="1" class="quantidade">
+            </div>
+            <div class="opcoesSabores-container"
+                <label
+                    for="opcao${x}">Sabores:
+                </label>
+                <select id="opcao${x}" class="opcoesSabores">
                     <option value="">----</option>
-                    <option value="">${dados[x].sabor}</option>
+                    ${sabores.map(sabor => `<option value="${sabor}">${sabor}</option>`).join('')}
                 </select>
             </div>
         </div>
         <div class="botao-container">
             <button class="btn-comprar">Comprar</button>
             <button class="btn-carrinho"><i class="fa fa-shopping-cart"></i></button>
+        </div>
         </div>`
-
-
-
-
-            // 3- adicionar os demais elementos dentro de section
         }
     }
 }
